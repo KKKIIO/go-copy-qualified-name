c5103f0dac22b12f0371d7bc50bd9474472a48a3
GitHub Action <action@github.com>
2023-10-31 12:46:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/kkkiio/gocopyqualifiedname/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/kkkiio/gocopyqualifiedname/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kkkiio.gocopyqualifiedname.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
